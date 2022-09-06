784ca0cb2b5df4f339e5f44110ae14439e0fdb69
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-09-06 18:42:18 -0400

    update readme for download

--- client/src/downloader/README.tsx
+++ client/src/downloader/README.tsx
@@ -1,7 +1,9 @@
 export const readmeText = `1. Please use the following comment to install required packages:
     npm i @react-three/cannon @react-three/drei react-three-fiber three
 
-2. Move the model folder into the src folder in your React App.
+2. Move the model.json into the public folder in your React App.
+
+2. Move the ShowModel.jsx or ShowModel.tsx file into the src folder
 
 3. import the ShowModel component to the place you want the model. 
   `;
