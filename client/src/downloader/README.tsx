98d575f525de05050dce35c674bd433aab20481d
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-09-06 19:25:27 -0400

    update guide on download readme

--- client/src/downloader/README.tsx
+++ client/src/downloader/README.tsx
@@ -1,9 +1,15 @@
 export const readmeText = `1. Please use the following comment to install required packages:
+    npm i @react-three/cannon @react-three/drei react-three-fiber three
+
+    -- for typescript users --
+
     npm i @react-three/cannon @react-three/drei react-three-fiber three @types/three
 
 2. Move the model.json into the public folder in your React App.
 
-2. Move the ShowModel.jsx or ShowModel.tsx file into the src folder
+3. Move the ShowModel.jsx or ShowModel.tsx file into the src folder
 
-3. import the ShowModel component to the place you want the model. 
+4. import the ShowModel component to the place you want the model. 
+
+5. npm start, and you will see the model on your webpage!
 `;
