32dbb5ee9b70660c033ab0ab19efda646c4c9e9d
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-09-07 23:30:53 -0400

    update user project migrate

--- db/migrate/20220830162725_create_user_projects.rb
+++ db/migrate/20220830162725_create_user_projects.rb
@@ -3,9 +3,9 @@ class CreateUserProjects < ActiveRecord::Migration[7.0]
     create_table :user_projects do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :project, null: false, foreign_key: true
-      t.boolean :favored
-      t.boolean :allow_edit
-      t.boolean :purchased
+      t.boolean :favored, default: false
+      t.boolean :allow_edit, default: false
+      t.boolean :purchased, default: false
 
       t.timestamps
     end
