bdafac913ab1ef9651b047d68936bdd22e3feb83
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-09-14 11:37:34 -0400

    remove unused import in setting control

--- client/src/components/DesignPage/DesignControls/SettingControls.tsx
+++ client/src/components/DesignPage/DesignControls/SettingControls.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import { ChromePicker, SliderPicker } from "react-color";
+import { ChromePicker } from "react-color";
 import { IProject, ISetting } from "../../../Interface";
 
 interface Props {
