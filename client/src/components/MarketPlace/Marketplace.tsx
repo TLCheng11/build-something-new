60c527546d84aea6e881e94f36064e0907a1dd72
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-09-14 19:31:49 -0400

    will show title on marketplace even if no project

--- client/src/components/MarketPlace/Marketplace.tsx
+++ client/src/components/MarketPlace/Marketplace.tsx
@@ -49,23 +49,23 @@ function MarketPlace() {
   }, [pageCount, currentPage]);
 
   return (
-    <div id="dashboard" className="flex h-screen w-screen bg-white text-black">
+    <div id="dashboard" className="h-screen w-screen bg-white text-black">
+      <div className="h-1/8 w-full flex justify-center">
+        <h1 id="header-title">
+          <span>B</span>
+          <span>U</span>
+          <span>I</span>
+          <span>L</span>
+          <span>D</span>
+        </h1>
+      </div>
       {pageCount < 1 ? (
-        <div className="flex items-center justify-center h-full w-full">
+        <div className="flex items-center justify-center h-7/8 w-full">
           <h1 className="text-4xl font-medium">No project on market</h1>
         </div>
       ) : (
-        <div className="h-full w-full">
-          <div className="h-1/8 w-full flex justify-center">
-            <h1 id="header-title">
-              <span>B</span>
-              <span>U</span>
-              <span>I</span>
-              <span>L</span>
-              <span>D</span>
-            </h1>
-          </div>
-          <div id="market-place-showcase" className="h-7/8 w-full">
+        <div className="h-7/8 w-full">
+          <div id="market-place-showcase" className="h-full w-full">
             <div className="h-7/8 w-full overflow-auto">
               <ProjectShowcase type="market" projects={allProjects} />
             </div>
