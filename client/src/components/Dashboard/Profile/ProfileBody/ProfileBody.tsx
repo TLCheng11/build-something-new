d1496a8f5412cc210d97e053ae1d3436bc452a17
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-03-01 19:39:59 -0500

    wrap updateProfile function in useCallback in ProfileBody

--- client/src/components/Dashboard/Profile/ProfileBody/ProfileBody.tsx
+++ client/src/components/Dashboard/Profile/ProfileBody/ProfileBody.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { UserContext } from "../../../../contexts/UserContext";
 import ProfileEmail from "./ProfileEmail";
 import ProfileFirstLastNames from "./ProfileFirstLastNames";
@@ -28,22 +28,25 @@ function ProfileBody({
 }: Props) {
   const { currentUser, setCurrentUser } = useContext(UserContext);
 
-  function updateProfile(e: React.FormEvent<HTMLFormElement>, input = {}) {
-    e.preventDefault();
-    fetch(`/users/${currentUser.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(input),
-    }).then((res) => {
-      if (res.ok) {
-        res.json().then(setCurrentUser);
-      } else {
-        res.json().then((data) => alert(data.errors));
-      }
-    });
-  }
+  const updateProfile = useCallback(
+    (e: React.FormEvent<HTMLFormElement>, input = {}) => {
+      e.preventDefault();
+      fetch(`/users/${currentUser.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(input),
+      }).then((res) => {
+        if (res.ok) {
+          res.json().then(setCurrentUser);
+        } else {
+          res.json().then((data) => alert(data.errors));
+        }
+      });
+    },
+    []
+  );
 
   return (
     <div className="w-full flex flex-col items-center justify-center text-2xl">
